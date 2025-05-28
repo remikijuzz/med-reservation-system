b7f466c535f86956c5516582aaf976edd2c1bce8
Remigiusz <remigiusz.gawenda@gmail.com>
2025-05-28 12:17:17 +0200

    chore: initial commit with project skeleton
    Signed-off-by: Remigiusz <remigiusz.gawenda@gmail.com>

---
+++ src/main/java/org/example/medreservationsystem/MedReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.medreservationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedReservationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedReservationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/medreservationsystem/MedReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.medreservationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedReservationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
